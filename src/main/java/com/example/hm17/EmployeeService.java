4a485c5f66693bef6840367fe304256fb5cc0b11
Chika_Pyu <ya.sasha-mochalov1@yandex.ru>
2024-02-17 12:24:49 +0300

    ff

---
+++ src/main/java/com/example/hm17/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.example.hm17;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public interface EmployeeService {
+
+
+}
